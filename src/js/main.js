a24b8b278f7387d77c7f2ccd32bc797fb5c72bd4
Silich Alexander <124524007+sili41995@users.noreply.github.com>
2025-03-20 19:48:38 +0200

    fix icons

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,7 @@
-console.log('hello');
+const questionsListRef = document.querySelector('.js-help-page-questions-list');
+
+const onQuestionsListClick = (e) => {
+  e.target.closest('.js-help-page-questions-container').classList.toggle('is-hidden');
+};
+
+questionsListRef.addEventListener('click', onQuestionsListClick);
